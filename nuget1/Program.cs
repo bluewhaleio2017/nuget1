cea2251e6518fca0e087c07d0e39d6f9454ece4f
bluewhaleio2017 <bluewhaleio2017@gmail.com>
2017-01-24 06:21:20 +0000

    first

---
+++ nuget1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Base.NugetHello;
+
+namespace Base.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin: Base.ConsoleApplication.");
+            Example e = new Example();
+            e.WriteLicense();
+            Console.WriteLine(e.GetMessage());
+            Console.WriteLine("");
+            Console.WriteLine("End: Base.ConsoleApplication.");
+            Console.WriteLine("");
+            Console.WriteLine("Press anykey to exit.");
+            Console.ReadKey();
+        }
+    }
+}
